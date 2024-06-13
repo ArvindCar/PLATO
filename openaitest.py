bdb8e42b5944eb0794951b669bf39996b7567a9f
Arvind Car <cararvind@gmail.com>
2024-06-13 15:30:01 -0400

    Syncing

--- openaitest.py
+++ openaitest.py
@@ -26,7 +26,7 @@ def encode_image(image):
         image.save(buffer, format="PNG")
         return base64.b64encode(buffer.getvalue()).decode('utf-8')
   
-image_path = "Trials/Arm_no_tool_1.png"
+image_path = "Trials/Grid_2.png"
 
 base64_image = encode_image(resize_and_return_image(image_path))
 
@@ -35,7 +35,30 @@ client = OpenAI()
 prompt = [
     {
         "role": "system", 
-        "content": "You are tasked with checking if a parallel plate gripper has successfully grasped a tool. If the tool is currently held by the gripper, reply 'Yes'. If the tool is not held by the gripper, reply 'No'."
+        "content": """You are tasked with observing an image of a parallel plate gripper in the process of grasping a tool. 
+                      You will be provided a side-view image of the scene, with a green grid superimposed on top of it. 
+                      Grid Description:
+                      Rows are represented by numbers (1-6) and columns are represented by alphabets (A-F). 
+                      Each Cell position in the grid is represented by a combination of the column letter and row number. (For example, B3).
+
+                      Your reply should contain three parts:
+                        Part 1: Location of the gripper:
+                          Your reply should be a cell position (for example B3). 
+                          Assume that the purple dot is representative of the current gripper location.
+                          Assume that the cyan dot is representative of the current object location.
+
+                        Part 2: Is the grasp successful?
+                          If the tool is currently held by the gripper, reply 'Yes'. 
+                          If the tool is not held by the gripper, reply 'No'.
+
+                        Part 3: Further action
+                          If your reply to Part2 is 'No', use the grid to tell me which cell to go to in order to be able to grasp the object.
+                          If your reply to Part2 is 'Yes', reply with 'None'
+                      
+                      Overall your answer should be of the format:
+                      <cell>
+                      <yes/no>
+                      <cell/None>"""
     },
     {
         "role": "user",
@@ -43,7 +66,7 @@ prompt = [
         [
           {
             "type": "text",
-            "text": "Is the robot grasping the tool? (Yes/No)"
+            "text": "Is the robot grasping the tool?"
           },
           {
             "type": "image_url",
